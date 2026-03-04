cc1987017acf2455712d085455c7c5cbe203cc52
Thibaut DEBREUX <thibaut.debreux7@etu.univ-lorraine.fr>
2026-03-04 10:46:57 +0100

    V1 interface web

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const mqtt = require('mqtt');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const mqttClient = mqtt.connect('mqtt://100.64.177.74:1880');
+
+mqttClient.on('connect', () => {
+    console.log('Connecté à Mosquitto');
+    mqttClient.subscribe('gyro_data');
+});
+
+mqttClient.on('message', (topic, message) => {
+    const data = message.toString();
+    console.log(`Donnée reçue sur ${topic}: ${data}`);
+
+    // On envoie la donnée en temps réel au navigateur
+    io.emit('donnee-iot', { val: data, date: new Date().toLocaleTimeString() });
+});
+
+app.use(express.static('public'));
+
+server.listen(3000, () => {
+    console.log('Serveur lancé sur http://localhost:3000');
+});
